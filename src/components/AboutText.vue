78f67c8ac510d9d9612cee58c18b89f7ba6c6fa5
KristinL112 <144241150+KristinL112@users.noreply.github.com>
2024-02-22 14:34:41 +0100

    Footer

---
+++ src/components/AboutText.vue
@@ -0,0 +1,25 @@
+<script>
+</script>
+
+<template>
+    <div id="About">
+    <v-card title= "PRISMA"
+    text="din digitala destination för att förvandla ditt hem till en färgsprakande oas. 
+        Med ett omfattande utbud av färger och nyanser, är denna e-handel 
+        dedikerad till att göra färgval till en smidig och inspirerande upplevelse.
+        
+        Teamet bakom PRISMA är inte bara experter på färg, utan de delar också regelbundet tips och trender genom blogginlägg och online-workshops. 
+        Detta gör det möjligt för kunder att inte bara köpa färg, utan också att få inspiration och kunskap för att skapa unika och personliga utrymmen.
+        Med snabba leveranser och en kundtjänst som alltid är redo att hjälpa till, strävar PRISMA efter att göra färgköp online till en smidig och givande upplevelse. 
+        Så oavsett om du är en inredningsentusiast eller bara letar efter att fräscha upp ditt hem, är PRISMA din pålitliga partner för att ge liv åt dina visioner med ett klick." 
+        variant="tonal">
+    </v-card>
+</div>
+</template>
+
+<style scoped>
+#About {
+margin: 2vh;
+padding: 2vh;
+}
+</style>
