5fd34f430c03aa194988e16edb9dd21d76e5013c
Beatrice Gyllenhammar <gyllenhammarbeatrice@gmail.com>
2024-02-22 14:48:54 +0100

    karusell

---
+++ src/components/ImgWheel.vue
@@ -0,0 +1,46 @@
+<template>
+    <v-container id="wheel">
+<v-carousel hide-delimiters :show-arrows="false">
+  <v-carousel-item id="wheelItem">
+<img src="../assets/images/testcat.jpg" alt=""
+cover>
+  </v-carousel-item>
+</v-carousel>
+
+<v-carousel hide-delimiters :show-arrows="false">
+  <v-carousel-item id="wheelItem">
+<img src="../assets/images/testcat.jpg" alt=""
+cover>
+  </v-carousel-item>
+</v-carousel>
+
+<v-carousel hide-delimiters :show-arrows="false">
+  <v-carousel-item id="wheelItem">
+<img src="../assets/images/testcat.jpg" alt=""
+cover>
+  </v-carousel-item>
+</v-carousel>
+
+<v-carousel hide-delimiters :show-arrows="false">
+  <v-carousel-item id="wheelItem">
+<img src="../assets/images/testcat.jpg" alt=""
+cover>
+  </v-carousel-item>
+</v-carousel>
+    </v-container>
+
+</template>
+
+<style scoped>
+#wheel {
+  margin:2vh;
+  display: flex;
+  justify-content: flex-end;
+}
+
+img{
+  margin:3vh;
+  width: 100%;
+  height: 100%;
+}
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -2,12 +2,14 @@
 import HeroImage from "../components/HeroImage.vue";
 import TestFetch from "@/components/TestFetch.vue";
 import About from "../components/AboutText.vue";
+import Carousel from "../components/ImgWheel.vue"
 </script>
 
 <template>
   <main>
     <TestFetch />
     <HeroImage />
+    <Carousel />
     <About />
   </main>
 </template>
