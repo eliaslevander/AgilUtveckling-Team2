d2f063aef73335b2ca040c879b22a24e32d539bf
Bearbetas <144781445+Bearbetas@users.noreply.github.com>
2024-02-27 14:09:23 +0100

    searchlist responsiv

--- src/components/SearchComponent.vue
+++ src/components/SearchComponent.vue
@@ -1,6 +1,5 @@
 <template>
   <!-- SÖKRUTA -->
-<<<<<<< HEAD
     <div id="searchbox">
         <v-text-field id="searchbar" label="Sök" 
         variant="outlined" 
@@ -23,7 +22,7 @@
         :key="items.id">
         <RouterLink id="searchLink" to="/">
         <BlobComponent :color="items.colorHex" />
-        {{ items.name }} 
+        <p>{{ items.name }} </p>
       </RouterLink>
       </v-list-item>
       </v-list-item-group>
@@ -68,86 +67,33 @@
       // value används för att komma år variabler
   </script>
    
-=======
-  <div id="searchbox">
-    <v-text-field
-      id="searchbar"
-      label="Sök"
-      variant="outlined"
-      v-model="searchInput"
-      placeholder="Sök produkter..."
-    >
-    </v-text-field>
-    <div v-for="items in store.products" :key="items.id" :title="items.name">
-      <ul>
-        <li>{{ items.image }} {{ items.name }}</li>
-      </ul>
-    </div>
-    <!-- SÖKKNAPP-->
-    <v-btn @click="searchResults"> Show </v-btn>
-  </div>
-</template>
-
-<script setup>
-// Composition api
-
-import { productsStore } from "../stores/products";
-import { ref, watch } from "vue";
-//  import { computed, ref, watch } from 'vue'
-// importera ref
-// importera computed (beräkande egenskap)
-// importera watch
-
-let searchInput = ref("");
-const store = productsStore();
-
-const filteredProducts = ref([]);
-
-watch(searchInput, () => {
-  filteredProducts.value = store.products.filter((product) =>
-    product.name.toUpperCase().includes(searchInput.value.toUpperCase())
-  );
-});
-
-function searchResults() {
-  store.products = filteredProducts.value;
-}
-
-// Definiera variabler
-
-// watch() =<{
-//Inom watch/paranteserna tar man in det man lyssnar på för att sen returnera
-
-// }
-
-//  const searchResults = itemName => {
-//    router.push({ name: 'product', params: { name: itemName } })
-//  }
-
-// value används för att komma år variabler
-</script>
->>>>>>> 94491e478101979f2325ea02451ab2c748433cbd
 
 <style scoped>
 
+button {
+  padding: 1%;
+}
 
 #blob {
-  width: 5vh;
+  width: 4vh;
   padding: 1vh;
   aspect-ratio: 1;
   border-radius: 60% 40% 44% 56% / 55% 43% 57% 45%;
-
-  position: relative;
+  position:relative;
 }
 
 #searchLink{
   text-decoration: none;
+  width: 300px;
   margin: auto;
   padding: 2vh;
   color: black;
   display: flex;
   align-items: center;
-  justify-content: center;
+  justify-content:flex-start;
+}
+p {
+  margin: 2vh;
 }
 
 #searchbox {
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,24 +1,15 @@
 <script setup>
 import HeroImage from "../components/HeroImage.vue";
-<<<<<<< HEAD
-=======
 import SearchResults from "../components/SearchComponent.vue";
->>>>>>> 94491e478101979f2325ea02451ab2c748433cbd
 import About from "../components/AboutText.vue";
-import Testfetch from "../components/TestFetch.vue"
 import Carousel from "../components/ImgWheel.vue";
 import TestFetch from "../components/TestFetch.vue";
 </script>
 
 <template>
   <main>
-<<<<<<< HEAD
-<Testfetch />
-    <SearchBar />
-=======
     <TestFetch />
     <SearchResults />
->>>>>>> 94491e478101979f2325ea02451ab2c748433cbd
     <HeroImage />
     <Carousel />
     <About />
