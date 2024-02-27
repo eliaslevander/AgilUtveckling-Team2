08707ed104774f08a2cab391ea1e8a523d0a31ae
Bearbetas <144781445+Bearbetas@users.noreply.github.com>
2024-02-27 13:42:32 +0100

    search watch and blob

--- src/components/SearchComponent.vue
+++ src/components/SearchComponent.vue
@@ -1,31 +1,43 @@
 <template>
-    <!-- SÖKRUTA -->
+  <!-- SÖKRUTA -->
     <div id="searchbox">
         <v-text-field id="searchbar" label="Sök" 
         variant="outlined" 
         v-model="searchInput" 
-        placeholder="Sök produkter...">
-        </v-text-field>
-      <div
-        v-for="items in store.products"
-        :key="items.id"
-        :title="items.name">
-        <ul>
-        <li> {{ items.image }} {{ items.name }}</li>
-      </ul>
-       </div>
-       <!-- SÖKKNAPP-->
-        <v-btn @click="searchResults"> 
-            Show
-        </v-btn>
+        placeholder="Sök produkter..."
+        ></v-text-field>
+  <!------->
+
+  <!-- SÖKKNAPP-->
+        <v-btn @click="searchResults"> Show </v-btn>
     </div>
-    
-  </template>
+  <!------->
+
+  <!-- SÖKRESULTAT -->
+        <span v-if="filteredProducts.length > 0"> </span>
+        <v-list class="blobcontainer">
+      <v-list-item-group>
+        <v-list-item 
+          v-for="items in filteredProducts"
+        :key="items.id">
+        <RouterLink id="searchLink" to="/">
+        <BlobComponent :color="items.colorHex" />
+        {{ items.name }} 
+      </RouterLink>
+      </v-list-item>
+      </v-list-item-group>
+    </v-list>
+  <!------->  
+      </template>
    
+
+
+
   <script setup>
       // Composition api
    
       import { productsStore } from '../stores/products'
+      import BlobComponent from "./BlobComponent.vue";
       import { ref, watch } from 'vue'
       //  import { computed, ref, watch } from 'vue'
       // importera ref
@@ -35,28 +47,18 @@
    
     let searchInput = ref('');
     const store = productsStore()
-
     const filteredProducts = ref([]);
 
     watch(searchInput, () => {
-  filteredProducts.value = store.products.filter((product) =>
+    filteredProducts.value = store.products.filter((product) =>
     product.name.toUpperCase().includes(searchInput.value.toUpperCase())
   );
 });
-
-
-function searchResults() {
-store.products = filteredProducts.value;
+    function searchResults() {
+  filteredProducts.value = store.products.filter((product) =>
+    product.name.toUpperCase().includes(searchInput.value.toUpperCase())
+  );
 }
-   
-      // Definiera variabler
-
-  
-   
-      // watch() =<{
-      //Inom watch/paranteserna tar man in det man lyssnar på för att sen returnera
-   
-      // }
    
       //  const searchResults = itemName => {
       //    router.push({ name: 'product', params: { name: itemName } })
@@ -67,8 +69,29 @@ store.products = filteredProducts.value;
    
 
 <style scoped>
+
+
+#blob {
+  width: 5vh;
+  padding: 1vh;
+  aspect-ratio: 1;
+  border-radius: 60% 40% 44% 56% / 55% 43% 57% 45%;
+
+  position: relative;
+}
+
+#searchLink{
+  text-decoration: none;
+  margin: auto;
+  padding: 2vh;
+  color: black;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+
 #searchbox {
-  width: 500px;
+  width: 200px;
   margin: auto;
   margin-top: 5vh;
 }
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,14 +1,14 @@
 <script setup>
 import HeroImage from "../components/HeroImage.vue";
-
 import About from "../components/AboutText.vue";
+import Testfetch from "../components/TestFetch.vue"
 import Carousel from "../components/ImgWheel.vue";
 import SearchBar from "../components/SearchComponent.vue"
 </script>
 
 <template>
   <main>
-
+<Testfetch />
     <SearchBar />
     <HeroImage />
     <Carousel />
