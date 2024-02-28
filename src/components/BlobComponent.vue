d22e20bcced5583c9096a504e07fcedbc7ca10e2
Elias Levander <elias.levander1@gmail.com>
2024-02-28 14:06:50 +0100

    gjorde skuggor responsiva

--- src/components/BlobComponent.vue
+++ src/components/BlobComponent.vue
@@ -1,31 +1,64 @@
 <template>
   <div
+    ref="blob"
     id="blob"
     :style="{
       backgroundColor: props.color,
       width: props.width,
       margin: props.margin,
+      // Detta gör skuggorna responsiva, istället för pixelvärde så utgår den från bredden på parent.
+      boxShadow: `
+      ${parentWidth * -0.03}px 
+      ${parentWidth * 0.02}px 
+      ${parentWidth * 0.04}px 
+      rgba(0, 0, 0, 0.5), 
+      inset 
+      ${parentWidth * 0.04}px 
+      ${parentWidth * -0.04}px 
+      ${parentWidth * 0.08}px  
+      rgba(0, 0, 0, 0.2)`,
     }"
   ></div>
 </template>
 
 <script setup>
+import { ref, onMounted } from "vue";
+
 const props = defineProps({
   color: String,
   width: String,
   margin: String,
 });
+
+const blob = ref(null);
+// Detta gör skuggorna responsiva, istället för pixelvärde så utgår den från bredden på parent.
+const parentWidth = ref(null);
+
+onMounted(() => {
+  const el = blob.value;
+  const computedStyle = window.getComputedStyle(el);
+  parentWidth.value = parseFloat(computedStyle.getPropertyValue("width"));
+});
+//-------------------------------------------------------------------------------------------------
 </script>
 
 <style scoped>
 #blob {
+  --default: 100px;
+
+  /* width: var(--default); */
   /*https://stackoverflow.com/questions/14061104/css3-box-shadow-size-percent-units*/
   width: inherit;
   aspect-ratio: 1;
   border-radius: 60% 40% 44% 56% / 55% 43% 57% 45%;
-  /* box-shadow: ; */
-  box-shadow: -4px 3px 5px rgba(0, 0, 0, 0.5),
-    inset 5px -5px 10px rgba(0, 0, 0, 0.2);
+  /* box-shadow: calc(var(--default) * -0.04) calc(var(--default) * 0.03)
+      calc(var(--default) * 0.05) rgba(0, 0, 0, 0.5),
+    inset calc(var(--default) * 0.05) calc(var(--default) * -0.05)
+      calc(var(--default) * 0.1) rgba(0, 0, 0, 0.2); */
+  /* box-shadow: -4px 3px 5px rgba(0, 0, 0, 0.5),
+    inset 5px -5px 10px rgba(0, 0, 0, 0.2); */
+  /* box-shadow: -4px 3px 5px rgba(0, 0, 0, 0.5),
+    inset 5px -5px 10px rgba(0, 0, 0, 0.2); */
   position: relative;
 }
 
