ddbfb064caf8847fef890074d1826c2d0a827998
William Falk <122986450+wi-fa@users.noreply.github.com>
2024-02-28 21:14:21 +0100

    lite nytt

---
+++ src/components/CartComponent.vue
@@ -0,0 +1,180 @@
+<script setup>
+    import { ref } from 'vue'
+    import { useCartStore } from '../stores/cart'
+
+    const cartStore = useCartStore()
+
+    const quantity = ref(1)
+
+    function increment() {
+        quantity.value++
+    }
+
+    function decrement() {
+        if (quantity.value > 1) {
+            quantity.value--
+        }
+    }
+</script>
+
+<template>
+    <div class="backdrop" v-show="cartStore.isCartVisible" @click="cartStore.closeCart"></div>
+    <div class="cart-container">
+        <div class="cart-modal" :class="{ 'cart-visible': cartStore.isCartVisible }">
+            <div class="cart-header">
+                <h3>Din Kundvagn</h3>
+                <button @click="cartStore.closeCart">X</button>
+            </div>
+            <hr>
+            <div class="cart-main">
+                <div id="preview-header">
+                    <h4>Produkt-titel</h4>
+                </div>
+                <div id="cart-content">
+                    <img src="/images/testcat.jpg" alt="" />
+                    <div class="quantity-selector">
+                        <button @click="decrement">-</button>
+                        <input v-model="quantity" readonly />
+                        <button @click="increment">+</button>
+                    </div>
+                    <p>Pris: 100kr</p>
+                </div>
+                <hr id="total-line">
+                <div id="cart-total">
+                    <h3>Summa</h3>
+                    <h3>123 kr</h3>
+                </div>
+                <div id="cart-checkout">
+                    <button id="to-checkout-btn">TILL KASSAN</button>
+                </div>
+                <hr>
+            </div>
+        </div>
+    </div>
+</template>
+
+<style scoped>
+
+
+    .backdrop {
+        position: fixed;
+        top: 0;
+        left: 0;
+        width: 100%;
+        height: 100%;
+        background-color: rgba(0, 0, 0, 0.5);
+        z-index: 100;
+    }
+
+    .cart-container {
+        display: flex;
+        justify-content: flex-end;
+        z-index: 109;
+    }
+    .cart-modal {
+        margin-right: 0rem;
+        width: 30rem;
+        height: 93vh;
+        background-color: rgb(255, 255, 255);
+        border: 1px solid black;
+        position: fixed;
+        top: 4rem;
+        right: 0rem;
+        z-index: 1050;
+        transition: transform 0.3s ease-in-out;
+        transform: translateX(100%);
+    }
+
+    .cart-modal h3{
+        font-weight: bold;
+    }
+
+    .cart-visible {
+    transform: translateX(0);
+}
+
+    hr {
+        width: 93%;
+        margin: auto;
+        opacity: 0.3;
+    }
+
+    .cart-header {
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+        padding: 1rem;
+    }
+
+    .cart-modal .cart-header a {
+        text-decoration: none;
+        color: black;
+    }
+
+    #preview-header {
+        display: flex;
+        justify-content: space-between;
+        padding: 1rem;
+    }
+
+    .quantity-selector {
+        background-color: whitesmoke;
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+    }
+
+    .quantity-selector button {
+        background-color: rgb(176, 176, 176);
+        width: 2rem;
+        margin: 0;
+        /* border: .5px solid black; */
+    }
+
+    .quantity-selector input {
+        margin: 0;
+        width: 2rem;
+        text-align: center;
+    }
+
+    #cart-content {
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+        padding: 1rem;
+    }
+
+    #cart-content img {
+        width: 6rem;
+        height: 6rem;
+    }
+
+    #cart-total {
+        display: flex;
+        justify-content: space-between;
+        padding: 1rem;
+    }
+
+    #cart-checkout {
+        display: flex;
+        justify-content: center;
+        padding: 1rem;
+    }
+
+    #to-checkout-btn {
+        font-size: 20px;
+        letter-spacing: .1rem;
+        width: 100%;
+        margin: auto;
+        padding: 1rem;
+        background-color: rgba(0, 0, 0, 0);
+        color: black;
+        border: 1px solid black;
+    }
+
+    @media screen and (max-width: 375px){
+    .cart-modal{
+        width: 20rem;
+    }
+}
+</style>
