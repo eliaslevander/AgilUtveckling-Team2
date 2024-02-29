e391eddd78ef8b96c417cd54dc007300acfee8a3
Elias Levander <elias.levander1@gmail.com>
2024-02-29 16:14:20 +0100

    commit

--- src/components/CoolBlob.vue
+++ src/components/CoolBlob.vue
@@ -1,5 +1,31 @@
 <template>
-  <BlobComponent :color="`rgb(${r}, ${g}, ${b})`" />
+  <div id="main-container">
+    <div id="blobs">
+      <div id="coolblob-container">
+        <BlobComponent
+          width="300px"
+          @click="clickForColor"
+          :color="`rgb(${r}, ${g}, ${b})`"
+        />
+        <div class="hex-container">
+          <p>R {{ r }}</p>
+          <p>G {{ g }}</p>
+          <p>B {{ b }}</p>
+        </div>
+      </div>
+      <div id="blob-container">
+        <BlobComponent
+          width="300px"
+          :color="`rgb(${clickedR}, ${clickedG}, ${clickedB})`"
+        />
+        <div class="hex-container">
+          <p>R {{ clickedR }}</p>
+          <p>G {{ clickedG }}</p>
+          <p>B {{ clickedB }}</p>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script setup>
@@ -7,23 +33,62 @@ import BlobComponent from "./BlobComponent.vue";
 
 import { ref, onMounted } from "vue";
 
-const r = ref(205);
-const g = ref(134);
-const b = ref(13);
+const r = ref(255);
+const g = ref(30);
+const b = ref(0);
+
+let clickedR = null;
+let clickedG = null;
+let clickedB = null;
+
+const clickForColor = () => {
+  clickedR = r.value;
+  clickedG = g.value;
+  clickedB = b.value;
+  console.log(clickedR, clickedG, clickedB);
+};
+
 const setupColorOscillation = (colorRef, speed) => {
   setInterval(() => {
     colorRef.value = Math.round(
-      127 * Math.sin((Date.now() / speed) * 0.001) + 128
+      127 * Math.sin((Date.now() / speed) * 0.1) + 128
     );
   }, speed);
 };
 
 onMounted(() => {
   // Set up color oscillations with different speeds
-  setupColorOscillation(r, 1000); // Adjust the speed as needed
+  setupColorOscillation(r, 100); // Adjust the speed as needed
   setupColorOscillation(g, 500);
   setupColorOscillation(b, 200);
 });
 </script>
 
-<style></style>
+<style>
+#main-container {
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+}
+
+#coolblob-container {
+  width: 400px;
+}
+
+#blob-container {
+  width: 400px;
+}
+
+#blobs {
+  display: flex;
+}
+
+.hex-container {
+  display: flex;
+  justify-content: space-evenly;
+  font-size: 1.25rem;
+  gap: 10px;
+}
+</style>
