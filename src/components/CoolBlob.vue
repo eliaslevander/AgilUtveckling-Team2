48359ae57511e7d593a60afd0074e66c7aa443d0
eliaslevander <144018111+eliaslevander@users.noreply.github.com>
2024-02-29 17:41:18 +0100

    Update CoolBlob.vue

--- src/components/CoolBlob.vue
+++ src/components/CoolBlob.vue
@@ -48,8 +48,7 @@ const setupColorOscillation = (colorRef, speed) => {
 };
 
 onMounted(() => {
-  // Set up color oscillations with different speeds
-  setupColorOscillation(r, 100); // Adjust the speed as needed
+  setupColorOscillation(r, 100); 
   setupColorOscillation(g, 500);
   setupColorOscillation(b, 200);
 });
