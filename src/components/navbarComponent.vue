bfd4f117fd9817649df6d4cd6c0d0ceb3d4a842f
KristinL112 <144241150+KristinL112@users.noreply.github.com>
2024-03-01 13:35:09 +0100

    Lagt in sök-knapp

--- src/components/navbarComponent.vue
+++ src/components/navbarComponent.vue
@@ -1,86 +1,86 @@
 <script>
-import { RouterLink } from "vue-router";
-import { useCartStore } from "@/stores/cart";
-/* Ikoner */
-import SvgIcon from "@jamescoyle/vue-icon";
-import { mdiMagnify } from "@mdi/js";
-import { mdiHeartOutline } from "@mdi/js";
-import { mdiShoppingOutline } from "@mdi/js";
-import { mdiMenu } from "@mdi/js";
-import { mdiClose } from "@mdi/js";
-import { mdiMenuDown } from "@mdi/js";
-import { mdiMenuRight } from "@mdi/js";
-import SearchComponent from "./SearchComponent.vue";
+  import { RouterLink } from 'vue-router'
+  import { useCartStore } from '@/stores/cart'
+  /* Ikoner */
+  import SvgIcon from '@jamescoyle/vue-icon'
+  import { mdiMagnify } from '@mdi/js'
+  import { mdiHeartOutline } from '@mdi/js'
+  import { mdiShoppingOutline } from '@mdi/js'
+  import { mdiMenu } from '@mdi/js'
+  import { mdiClose } from '@mdi/js'
+  import { mdiMenuDown } from '@mdi/js'
+  import { mdiMenuRight } from '@mdi/js'
+  import SearchComponent from './SearchComponent.vue'
 
-//----------Funktionalitet för sök mobile/desktop--------------
+  //----------Funktionalitet för sök mobile/desktop--------------
 
-// OBS, reload av sidan krävs för att den ska kunna kolla mobile/dekstop
+  // OBS, reload av sidan krävs för att den ska kunna kolla mobile/dekstop
 
-// Hur vet den om den ska öppna mobil sök eller desktop sök?
-// checkIfMobile kollar om bredden är under 769 px bred, om den är under så sätts
-// this.isMobile till true, annars sätts den till false. Den körs när nav skapas under created.
+  // Hur vet den om den ska öppna mobil sök eller desktop sök?
+  // checkIfMobile kollar om bredden är under 769 px bred, om den är under så sätts
+  // this.isMobile till true, annars sätts den till false. Den körs när nav skapas under created.
 
-// Funktionen handleSearchComponent triggas vid click på sökikonen, den kollar värdet på isMobile.
-// Om false (desktopläge) så aktiveras "toggle" funktionalitet på this.showSearchComponent. Annars
-// så aktiveras den mobila menyns "toggle" funktionalitet.
+  // Funktionen handleSearchComponent triggas vid click på sökikonen, den kollar värdet på isMobile.
+  // Om false (desktopläge) så aktiveras "toggle" funktionalitet på this.showSearchComponent. Annars
+  // så aktiveras den mobila menyns "toggle" funktionalitet.
 
-export default {
-  components: {
-    SvgIcon,
-    SearchComponent,
-  },
-  created() {
-    this.checkIfMobile();
-  },
-  data() {
-    return {
-      magnifyPath: mdiMagnify,
-      heartPath: mdiHeartOutline,
-      shoppingPath: mdiShoppingOutline,
-      menuPath: mdiMenu,
-      closePath: mdiClose,
-      menuDownPath: mdiMenuDown,
-      menuRightPath: mdiMenuRight,
-      drawer: null,
-      isMobile: null,
-      search: "",
-      showDropdownMenu: false,
-      showColorsDropdown: false,
-      showSearchComponent: false,
-    };
-  },
-  methods: {
-    toggleCartVisibility() {
-      const cartStore = useCartStore();
-      cartStore.toggleCartVisibility();
+  export default {
+    components: {
+      SvgIcon,
+      SearchComponent
     },
-    toggleDropdownMenu() {
-      this.showDropdownMenu = !this.showDropdownMenu;
+    created() {
+      this.checkIfMobile()
     },
-    toggleColorsDropdown() {
-      this.showColorsDropdown = !this.showColorsDropdown;
-    },
-    handleSearchComponent() {
-      if (!this.isMobile) {
-        this.showSearchComponent = !this.showSearchComponent;
-        console.log("Är mobil?", this.isMobile);
-        console.log("Öppna/stäng sök", this.showSearchComponent);
-      } else {
-        this.drawer = !this.drawer;
-        console.log(this.drawer);
+    data() {
+      return {
+        magnifyPath: mdiMagnify,
+        heartPath: mdiHeartOutline,
+        shoppingPath: mdiShoppingOutline,
+        menuPath: mdiMenu,
+        closePath: mdiClose,
+        menuDownPath: mdiMenuDown,
+        menuRightPath: mdiMenuRight,
+        drawer: null,
+        isMobile: null,
+        search: '',
+        showDropdownMenu: false,
+        showColorsDropdown: false,
+        showSearchComponent: false
       }
     },
-    checkIfMobile() {
-      let width = screen.width;
-      console.log(width);
-      if (width < 769) {
-        this.isMobile = true; /* Mobile */
-      } else {
-        this.isMobile = false; /* Desktop */
+    methods: {
+      toggleCartVisibility() {
+        const cartStore = useCartStore()
+        cartStore.toggleCartVisibility()
+      },
+      toggleDropdownMenu() {
+        this.showDropdownMenu = !this.showDropdownMenu
+      },
+      toggleColorsDropdown() {
+        this.showColorsDropdown = !this.showColorsDropdown
+      },
+      handleSearchComponent() {
+        if (!this.isMobile) {
+          this.showSearchComponent = !this.showSearchComponent
+          console.log('Är mobil?', this.isMobile)
+          console.log('Öppna/stäng sök', this.showSearchComponent)
+        } else {
+          this.drawer = !this.drawer
+          console.log(this.drawer)
+        }
+      },
+      checkIfMobile() {
+        let width = screen.width
+        console.log(width)
+        if (width < 769) {
+          this.isMobile = true /* Mobile */
+        } else {
+          this.isMobile = false /* Desktop */
+        }
       }
-    },
-  },
-};
+    }
+  }
 </script>
 
 <template>
@@ -219,49 +219,53 @@ export default {
 </template>
 
 <style scoped>
-.dropdown-content {
-  position: absolute;
-  margin-top: 64px;
-  width: 100vw;
-  height: 13rem;
-  background-color: #ffffff;
-  z-index: 1;
-}
-#brand {
-  color: #000000;
-  text-decoration: none;
-  font-size: 2rem;
-  margin-left: 2rem;
-}
-.navigation-link {
-  text-decoration: none;
-  color: #000000;
-}
-.navigation-link:hover {
-  text-decoration: underline;
-}
-.navigation-item {
-  text-decoration: none;
-  color: #000000;
-  display: flex;
-  align-items: center;
-  justify-content: center;
-}
-.subMenu {
-  position: absolute;
-  margin-left: 15rem;
-  margin-top: -43px;
-  background-color: #ffffff;
-  z-index: 1;
-  width: 10rem;
-}
-
-@media (max-width: 380px) {
-  #brand {
-    font-size: 1.5rem;
-  }
   .dropdown-content {
-    display: none;
+    position: absolute;
+    margin-top: 64px;
+    width: 100vw;
+    height: 13rem;
+    background-color: #ffffff;
+    z-index: 1;
   }
-}
+  #brand {
+    color: #000000;
+    text-decoration: none;
+    font-size: 2rem;
+    margin-left: 2rem;
+  }
+  .navigation-link {
+    text-decoration: none;
+    color: #000000;
+  }
+  .navigation-link:hover {
+    text-decoration: underline;
+  }
+  .navigation-item {
+    text-decoration: none;
+    color: #000000;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+  }
+  .subMenu {
+    position: absolute;
+    margin-left: 15rem;
+    margin-top: -43px;
+    background-color: #ffffff;
+    z-index: 1;
+    width: 10rem;
+  }
+
+  .desktopSearch {
+    background-color: #f5f5f5;
+  }
+
+  @media (max-width: 380px) {
+    #brand {
+      font-size: 1.5rem;
+    }
+    .dropdown-content {
+      display: none;
+    }
+  }
 </style>
