bd4faf439a7a461bd973983653fde564e0d525c9
Elias Levander <elias.levander1@gmail.com>
2024-04-25 00:22:49 +0200

    fixar inför vercel deploy

--- src/components/FooterComponent.vue
+++ src/components/FooterComponent.vue
@@ -8,7 +8,7 @@
         >
           <img
             style="width: 100%"
-            src="../assets/images/Logo_NEW.png"
+            src="../assets/images/logo_new.png"
             alt=""
           /></div
       ></router-link>
@@ -33,52 +33,52 @@
   </v-footer>
 </template>
 <script>
-  // import { RouterLink } from 'vue-router'
-  export default {
-    data: () => ({
-      links: ['Kontakt', 'Vanliga frågor', 'Samarbeten', 'Användarvillkor']
-    })
-  }
+// import { RouterLink } from 'vue-router'
+export default {
+  data: () => ({
+    links: ["Kontakt", "Vanliga frågor", "Samarbeten", "Användarvillkor"],
+  }),
+};
 </script>
 
 <style scoped>
-  .v-btn {
-    font-size: 1.1rem;
-    padding-top: 0.1rem;
+.v-btn {
+  font-size: 1.1rem;
+  padding-top: 0.1rem;
+}
+
+#logo-container {
+  padding-top: 0.2rem;
+  margin-left: 1rem;
+}
+
+@media screen and (max-width: 850px) {
+  .v-row {
+    display: block;
+    height: 38vh;
+  }
+
+  #logo-container img {
+    max-width: 115px;
   }
 
   #logo-container {
-    padding-top: 0.2rem;
-    margin-left: 1rem;
+    padding-top: 1rem;
+    padding-bottom: 1rem;
+    margin-left: 0.7rem;
   }
 
-  @media screen and (max-width: 850px) {
-    .v-row {
-      display: block;
-      height: 38vh;
-    }
-
-    #logo-container img {
-      max-width: 115px;
-    }
-
-    #logo-container {
-      padding-top: 1rem;
-      padding-bottom: 1rem;
-      margin-left: 0.7rem;
-    }
-
-    .v-btn {
-      display: flex;
-      padding-left: 0.1rem;
-    }
-
-    p {
-      display: flex;
-      justify-content: left;
-      text-align: left;
-      padding-bottom: 1rem;
-      margin-left: 0.6rem;
-    }
+  .v-btn {
+    display: flex;
+    padding-left: 0.1rem;
   }
+
+  p {
+    display: flex;
+    justify-content: left;
+    text-align: left;
+    padding-bottom: 1rem;
+    margin-left: 0.6rem;
+  }
+}
 </style>
